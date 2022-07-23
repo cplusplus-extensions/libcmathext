5a2614188464cf4ccf24cddf13a35a1761eabed0
marck283 <86911602+marck283@users.noreply.github.com>
2022-07-23 18:36:48 +0200

    Modificata implementazione funzione gaussSum per non lanciare un'eccezione quando si fornisce il valore 0 in input

--- src/gauss/gauss.cpp
+++ src/gauss/gauss.cpp
@@ -13,13 +13,13 @@
  * @return long long int 
  */
 long long int Gauss::gaussSum(long long int value) {
-    if(value > 0) {
+    if(value >= 0) {
         try {
             return value*(value + 1)/2;
         } catch(std::overflow_error& e) {
-            throw new std::overflow_error("Arithmetic overflow occurred.");
+            throw std::overflow_error("Arithmetic overflow occurred.");
         }
     } else {
-        throw new std::invalid_argument("Input value cannot be negative or zero.");
+        throw std::invalid_argument("Input value cannot be negative or zero.");
     }
 }
